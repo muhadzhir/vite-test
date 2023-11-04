5637d7beec1c5e2bfda7bd973a322bf7b9448779
kasty <kastiel_0991@mail.ru>
2023-11-04 11:52:44 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    <div class="test-child">test</div>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style.scss';
+
+.test {
+
+  background-color: red;
+
+  &-child {
+    color: rebeccapurple;
+  }
+}
+</style>
