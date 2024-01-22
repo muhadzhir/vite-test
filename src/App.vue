7812fe1f0ff976cc00af0911081842b1063898dd
kasty <kastiel_0991@mail.ru>
2024-01-22 11:20:29 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="test">
-    <div class="test-child">test</div>
+    <div class="test-child">{{ test }}</div>
   </div>
 </template>
-
+<script lang="ts" setup>
+const test = 'test'
+</script>
 <style lang="scss">
 .test {
   &-child {
